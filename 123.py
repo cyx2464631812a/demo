38d8a623f38a20749f7db56dff76af2a9486737a
cyx2464631812a <92005397+cyx2464631812a@users.noreply.github.com>
2021-10-18 11:07:49 +0800

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,129 @@
+# while True:
+#     A = float(input("请输入第一个数字"))
+#     B = float(input("请输入第二个数字"))
+#     C = float(input("请输入第三个数字"))
+#     D = float(input("请输入第四个数字"))
+#     E = float(input("请输入第五个数字"))
+#     F = float(input("请输入第六个数字"))
+#     G = float(input("请输入第七个数字"))
+#     H = float(input("请输入第八个数字"))
+#     I = float(input("请输入第九个数字"))
+#     J = float(input("请输入第十‘个数字"))
+#     K = A + B + C + D + E + F + G + H + I + J
+#     print("10个数字之和是",K)
+
+# print("\n")
+# while True:
+#     A = float(input("请输入第一个数字"))
+#     B = float(input("请输入第二个数字"))
+#     C = float(input("请输入第三个数字"))
+#     D = float(input("请输入第四个数字"))
+#     E = float(input("请输入第五个数字"))
+#     F = float(input("请输入第六个数字"))
+#     G = float(input("请输入第七个数字"))
+#     H = float(input("请输入第八个数字"))
+#     I = float(input("请输入第九个数字"))
+#     J = float(input("请输入第十‘个数字"))
+#     N = [A, B, C, D, E, F, G, H, I, J]
+#     K = A + B + C + D + E + F + G + H + I + J
+#     print(K)
+#     L = K / 10
+#     print(L)
+#     M = max(N)
+#     print(M)
+
+# print("\n")
+# import random
+# list = []
+# for i in range(50,100+1):
+#     print(list)
+#     list.append  (i)
+
+# print("\n")
+# while True:
+#     A = float(input("请输入边长a",))
+#     B = float(input("请输入边长b",)
+#     C = float(input("请输入边长c",))
+#     if A + B > C and B + C > A and A + C > B:
+#         print("可以形成三角形")
+#         if B == A == C:
+#             print("是等边三角形")
+#         elif A != B and A != C and B != C:
+#             print("是普通三角形")
+#         else:
+#             print("是等腰三角形")
+
+# print("\n")
+# A = input("请输入A的值")
+# B = input("请 输入B的值")
+# C = A
+# A = B
+# B = C
+# print("交换后A为：{}".format(A))
+# print("交换后B为：{}".format(B))
+
+# print("\n")
+# R = 0
+# while True:
+#     R = R + 1
+#     intA = "root"
+#     intB = "admin"
+#     print(intA)
+#     print(intB)
+#     a = str(input("请输入用户名"))
+#     b = str(input("请输入密码"))
+#     if R < 3:
+#         if a == intA and b == intB:
+#             print("登陆成功")
+#             break
+#         elif a != intA:
+#             print("用户名错误")
+#         elif b != intB:
+#             print("密码错误")
+#     else:
+#         print("输入错误3次功能已锁定")
+#         break
+
+# print("\n")
+# for i in range(8):
+#     print(" "*(7-i),end=" ")
+#     print("* "*(i+1))
+
+# print("\n")
+# R = 1
+# T = 1
+# N = 0
+# while N < 100:
+#     L = R * T
+#     print(R,"*",T,"=",L)
+#     T = T + 1
+#     R = R + 1
+#     N = N + 1
+
+# print("\n")
+# R = 9
+# T = 9
+# while True:
+#     L = R * T
+#     print(R,"*",T,"=",L)
+#     R = R - 1
+#     if R == 0:
+#         T = T - 1
+#         R = T
+#         if T == 0:
+#             break
+
+# print("\n")
+# T = 0
+# D = 1
+# while True:
+#     T = T + 3
+#     if T >= 20:
+#         print(D)
+#         break
+#     T = T - 2
+#     D = D + 1
+
+
+
+
